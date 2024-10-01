0bc8cdc53a0af896b27036f042879755abd8e83b
Mikaela-D <G00401392@atu.ie>
2024-10-01 20:27:04 +0100

    Added new project

---
+++ src/main/java/ie/atu/yr4lab2buildingtwointeractingmicroservices/Yr4Lab2BuildingTwoInteractingMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.yr4lab2buildingtwointeractingmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yr4Lab2BuildingTwoInteractingMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Yr4Lab2BuildingTwoInteractingMicroservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/yr4lab2buildingtwointeractingmicroservices/Yr4Lab2BuildingTwoInteractingMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.yr4lab2buildingtwointeractingmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Yr4Lab2BuildingTwoInteractingMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
